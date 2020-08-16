ba39fe1660b6928ebfe81df40200cdf7f8d474b9
Emmanuel Oseguera <emmanuel.oseguera@magmalabs.io>
2020-08-16 10:51:55 -0500

    Add a Dash of Stimulus in Posts
    - Add a dash of stimulus in posts_controller.js in order to send
      the message when the user hit the enter key. This because of
      the nature of textarea behaviour.
    - Set the right hooks in the DOM in order to make accesible for
      stimulus.

--- app/javascript/controllers/posts_controller.js
+++ app/javascript/controllers/posts_controller.js
@@ -1,37 +1,12 @@
-import ApplicationController from './application_controller'
+// import { Controller } from "stimulus";
+import ApplicationController from "./application_controller";
 
-/* This is the custom StimulusReflex controller for PostsReflex.
- * Learn more at: https://docs.stimulusreflex.com
- */
 export default class extends ApplicationController {
-  /* Reflex specific lifecycle methods.
-   * Use methods similar to this example to handle lifecycle concerns for a specific Reflex method.
-   * Using the lifecycle is optional, so feel free to delete these stubs if you don't need them.
-   *
-   * Example:
-   *
-   *   <a href="#" data-reflex="PostsReflex#example">Example</a>
-   *
-   * Arguments:
-   *
-   *   element - the element that triggered the reflex
-   *             may be different than the Stimulus controller's this.element
-   *
-   *   reflex - the name of the reflex e.g. "PostsReflex#example"
-   *
-   *   error - error message from the server
-   */
+  static targets = ["submitButton"];
 
-  // beforeUpdate(element, reflex) {
-  //  element.innerText = 'Updating...'
-  // }
-
-  // updateSuccess(element, reflex) {
-  //   element.innerText = 'Updated Successfully.'
-  // }
-
-  // updateError(element, reflex, error) {
-  //   console.error('updateError', error);
-  //   element.innerText = 'Update Failed!'
-  // }
+  sendMessageWhenEnter(e) {
+    if (e.key === "Enter" && e.shiftKey === false) {
+      this.submitButtonTarget.click();
+    }
+  }
 }
