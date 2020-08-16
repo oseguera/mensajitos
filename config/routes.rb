7f3c56815d703d490ac9cb9a3713d61dd4b28a7d
Emmanuel Oseguera <emmanuel.oseguera@magmalabs.io>
2020-08-16 18:13:10 -0500

    Add some routes for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :create]
+
+  get '/index.php', to: 'posts#index'
+  get '/index.html', to: 'posts#index'
+
   root "posts#index"
 end
